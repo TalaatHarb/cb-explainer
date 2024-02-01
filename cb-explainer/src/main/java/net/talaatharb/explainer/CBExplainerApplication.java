5bc5958910bbe78776ddfdd7b7fd20e5fcd7c2ad
mharb <mohamed.harb@cegedim.com>
2024-02-01 10:33:05 +0200

    modified the template to use project name cb-explainer

---
+++ cb-explainer/src/main/java/net/talaatharb/explainer/CBExplainerApplication.java
@@ -0,0 +1,10 @@
+package net.talaatharb.explainer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CBExplainerApplication{
+    public static void main(String[] args) {
+        log.info("Application Started");
+    }
+}
---
+++ cb-explainer/src/test/java/net/talaatharb/explainer/CBExplainerApplicationTest.java
@@ -0,0 +1,14 @@
+package net.talaatharb.explainer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class CBExplainerApplicationTest {
+
+	@Test
+	void testProjectStarts() {
+		CBExplainerApplication.main(null);
+		assertTrue(true);
+	}
+}
