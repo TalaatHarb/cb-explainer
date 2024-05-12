de23f62c63fb819a07b8e3b49408e162899f9650
mharb <mohamed.harb@cegedim.com>
2024-05-12 13:26:11 +0300

    upgraded library versions and removed unused source code

--- cb-explainer/src/main/java/net/talaatharb/explainer/JavafxApplication.java
+++ cb-explainer/src/main/java/net/talaatharb/explainer/JavafxApplication.java
@@ -1,19 +1,12 @@
 package net.talaatharb.explainer;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-
 import javafx.application.Application;
-import javafx.application.HostServices;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import net.talaatharb.explainer.ui.events.StageReadyEvent;
 
 public class JavafxApplication extends Application {
 
@@ -24,23 +17,8 @@ public class JavafxApplication extends Application {
 	private static final String TITLE = "Couchbase Explainer";
 	private static final int WIDTH = 800;
 
-	private ConfigurableApplicationContext context;
-
-	@Override
-	public void init() throws Exception {
-		final ApplicationContextInitializer<GenericApplicationContext> initializer = genericApplicationContext -> {
-			genericApplicationContext.registerBean(Application.class, () -> JavafxApplication.this);
-			genericApplicationContext.registerBean(Parameters.class, this::getParameters);
-			genericApplicationContext.registerBean(HostServices.class, this::getHostServices);
-		};
-
-		context = new SpringApplicationBuilder().sources(CBExplainerApplication.class).initializers(initializer)
-				.build().run(getParameters().getRaw().toArray(new String[0]));
-	}
-
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		context.publishEvent(new StageReadyEvent(primaryStage));
 
 		final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(MAIN_FXML));
 		final Parent root = fxmlLoader.load();
@@ -58,7 +36,6 @@ public class JavafxApplication extends Application {
 
 	@Override
 	public void stop() throws Exception {
-		context.close();
 		Platform.exit();
 	}
 
